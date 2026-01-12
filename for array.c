82e0a3bb4a397cc7af58626c4a32eb1c4aa58450
Reno Joby <jobyreno2007@gmail.com>
2026-01-12 15:50:50 +0000

    new file:   for array

--- for array.c
+++ for array.c
@@ -2,9 +2,20 @@
 int main(){
     //entering no of elements
     int n;
-    printf("enter the no. of elements:")
+    printf("enter the no. of elements:");
     scanf("%d",&n);
     int a[n],i;
     //storing in an array
-    
+    for(i=0;i<n;i++){
+        printf("enter number:");
+        scanf("%d",&a[i]);
+    }
+    //printing the array
+    printf("[");
+    for(i=0;i<n;i++){
+        printf("%d",a[i]);
+        printf(",");
+    }
+    printf("\b]");
+    return 0;
 }
